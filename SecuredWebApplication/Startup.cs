220f26d5671a1b756b042084d550d1a198a50cb2
jayinde <jamiuaz1@hotmail.co.uk>
2017-07-23 11:33:37 +0100

    set up

---
+++ SecuredWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecuredWebApplication.Startup))]
+namespace SecuredWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
